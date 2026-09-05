42d6923ad42cb95bd8a2e3912a93d9b46df5dc6c
Mkay <264876915+dudemkay@users.noreply.github.com>
2026-09-05 14:24:25 +0100

    test: skip db env validation in unit tests

--- jest.setup.tsx
+++ jest.setup.tsx
@@ -1,3 +1,5 @@
+process.env.SKIP_ENV_VALIDATION = process.env.SKIP_ENV_VALIDATION ?? 'true';
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
