95f1ae6d632144faaea6b3d21f4e7778f5258624
Mkay <264876915+dudemkay@users.noreply.github.com>
2026-09-06 12:21:02 +0100

    feat: export automation webhook schema

--- src/shared/db/schema/index.ts
+++ src/shared/db/schema/index.ts
@@ -23,4 +23,6 @@ export * from './schema';
 export * from './tenants';
 export * from './webhooks';
 export * from './workflow-runs';
+export * from './workflow-webhook-deliveries';
+export * from './workflow-webhook-endpoints';
 export * from './workflows';
