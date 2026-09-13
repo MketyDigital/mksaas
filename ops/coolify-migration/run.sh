cb6f748c3a667078a7bea5391d89a6df93971816
Mkay <264876915+dudemkay@users.noreply.github.com>
2026-09-13 08:05:03 +0100

    test: require base production migration before health

--- ops/coolify-migration/run.sh
+++ ops/coolify-migration/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -eu
 
+pnpm db:migrate
+
 exec node -e "require('http').createServer((req,res)=>{res.statusCode=200;res.setHeader('content-type','text/plain');res.end('ok')}).listen(3000,'0.0.0.0')"
